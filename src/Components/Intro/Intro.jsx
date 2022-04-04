15847581ce4b9509d50bb04a0c621f858318e047
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-04 18:47:35 -0300

    I-right

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,12 @@ import './Intro.css';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import Boy from '../../img/boy.png';
+import Thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import Glassesimoji from '../../img/glassesimoji.png';
 
 const Intro = () => {
     return (
@@ -22,7 +28,11 @@ const Intro = () => {
 
             </div>
             <div className="i-right">
-                eu sou lado direito
+                
+                
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={Boy} alt="" />
             </div>
         </div>
     );
