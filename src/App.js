8ad3fdc39333ea270eb48bca366027902f61341f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-04 11:01:00 -0300

    README update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá, Mundo!... Sapup3 na Área.
         </p>
         <a
           className="App-link"
