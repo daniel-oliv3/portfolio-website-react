f8cf18480795aa00c6278e4428c22a87e52f1a0a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-04 17:49:04 -0300

    Intro update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar/Navbar";
+import Intro from "./Components/Intro/Intro";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
           <Navbar/>
+          <Intro/>
     </div>
   );
 }
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Intro = () => {
     return (
         <div className="intro">
-        
+            <div className="i-left">
+            <div className="i-name">
+                <span>Oi! Eu sou</span>
+                <span>Daniel Oliveira</span>
+                <span>Desenvolvedor frontend com alta experiência em web design e desenvolvimento, produzindo o trabalho de qualidade</span>
+            </div>
+            <button className="i-button">Contrate-me</button>
+
+            </div>
+            <div className="i-right"></div>
         </div>
     );
 }
