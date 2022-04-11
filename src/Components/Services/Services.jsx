3365ddea340a1f243e50ca88295a3b217c6ce46c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-11 10:53:39 -0300

    Update Services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import './Services.css';
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
 
 
 const Services = () => {
     return (
         <div className="services">
 
+            {/* Left side */}
+            <div className="awesome">
+            <span>Meu Incrível</span>
+            <span>Serviços</span>
+            <spane>
+                Quem num gosta di mim que vai caçá sua turmis! Manduma pindureta quium dia nois paga.
+                <br/>
+                Suco de cevadiss deixa as pessoas mais interessantis.
+            </spane>
+            <button className="button s-button">Downloads CV</button>
+            <div className="blur s-blur1" style={{background: "#ABF1FF94"}}></div>
+            </div>
+            {/* Right side */}
+            <div className="cards">
+                Lado direito
+            </div>
         </div>
     )
 }
