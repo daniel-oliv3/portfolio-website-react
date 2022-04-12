9fecaf40883127eb3d6f5d439499ee5b7b8a2f6c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-12 11:24:18 -0300

    Create folder and Components Portifolio.jsx e Portifolio.css

---
+++ src/Components/Portifolio/Portifolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Portifolio.css';
+
+
+const Portifolio = () => {
+    return (
+        <div className="portifolio">
+
+            
+        </div>
+    )
+}
+
+
+export default Portifolio;
