f431fc2e38ab76813a03d98c9f1f693afbdf2653
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-12 15:03:24 -0300

    Create folder and Components Testimonials.jsx e Testimonials.css

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Testimonials.css';
+
+
+const Testimonials = () => {
+    return (
+        <div className="testimonials">
+            
+        </div>
+    )
+}
+
+
+export default Testimonials;
