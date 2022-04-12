6cb146918355e949b140c5b9716577fa91421b65
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-12 09:21:48 -0300

    Update

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -4,8 +4,22 @@ import './Experience.css';
 
 const Experience = () => {
     return (
-        <div>
-
+        <div className="experience">
+            <div className="achievement">
+                <div className="circle">8+</div>
+                <span>Anos</span>
+                <span>Experiência</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">20+</div>
+                <span>Concluído</span>
+                <span>Projetos</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">5+</div>
+                <span>Empresas</span>
+                <span></span>
+            </div>
         </div>
     )
 }
