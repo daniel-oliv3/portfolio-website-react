0ec8a4c0267e2c3bace36652524bbde92ad9824b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-12 09:50:56 -0300

    Create folder and Components Works.jsx e Works.css

---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Works.css';
+
+
+const Works = () => {
+    return (
+        <div className="works">
+            
+        </div>
+    )
+}
+
+
+export default Works;
